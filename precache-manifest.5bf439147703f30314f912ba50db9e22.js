a035789d2653af5b403af12ae32c5c4e5ff8c885
Ritwik Sharma <42085720+RitwikSharma107@users.noreply.github.com>
2020-04-03 00:55:33 +0530

    Updates

---
+++ precache-manifest.5bf439147703f30314f912ba50db9e22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c78042eed682b01649fe9d4c6faa3d0",
+    "url": "/seven-wonders-of-the-world/index.html"
+  },
+  {
+    "revision": "21f0eb585fcaa8350321",
+    "url": "/seven-wonders-of-the-world/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c38d900291241a69398f",
+    "url": "/seven-wonders-of-the-world/static/css/main.c88e3457.chunk.css"
+  },
+  {
+    "revision": "21f0eb585fcaa8350321",
+    "url": "/seven-wonders-of-the-world/static/js/2.556fd944.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seven-wonders-of-the-world/static/js/2.556fd944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38d900291241a69398f",
+    "url": "/seven-wonders-of-the-world/static/js/main.74b2b3c9.chunk.js"
+  },
+  {
+    "revision": "33cea77b6988cb84674b",
+    "url": "/seven-wonders-of-the-world/static/js/runtime-main.c1bc6060.js"
+  }
+]);
